4e84da8f97296c0dd7af6dab219f254e5ba9bb8d
SlimTim10 <slimtim10@gmail.com>
2017-11-30 20:00:00 -0500

    Create layout and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import Navbar from './Navbar.jsx';
+import MessageList from './MessageList.jsx';
+import Chatbar from './Chatbar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+        <div>
+        <Navbar />
+        <MessageList />
+        <Chatbar />
+        </div>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+        <main className="messages">
+        <Message type="user" username="Anonymous1" content="I won't be impressed with technology until I can download food." />
+        <Message type="system" content="Anonymous1 changed their name to nomnom." />
+        </main>
+    );
+  }
+}
+export default MessageList;
