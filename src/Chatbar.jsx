810d5462ab3e5253676dd943498ad0db52341f4d
SlimTim10 <slimtim10@gmail.com>
2017-12-01 16:53:26 -0500

    Handle changing username

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -3,24 +3,31 @@ import React, {Component} from 'react';
 class Chatbar extends Component {
   constructor(props) {
     super(props);
+    this.state = {username: this.props.username};
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeUsername = this.changeUsername.bind(this);
   }
   
   handleSubmit(event) {
     if (event.charCode === 13) {
       event.preventDefault();
       const content = event.target.value;
-      this.props.sendMessage(this.props.username, content);
+      const username = this.state.username;
+      this.props.sendMessage(username, content);
       event.target.value = "";
     }
+  }
+
+  changeUsername(event) {
+    this.setState({username: event.target.value});
   }
   
   render() {
     console.log('Rendering <Chatbar />');
     return (
         <footer className="chatbar">
-	    <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.username} />
+	    <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.state.username} onChange={this.changeUsername} />
 	    <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.handleSubmit} />
         </footer>
     );
